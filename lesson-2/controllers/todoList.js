02e4a811dbb9007ebe06611638dd8b4be6d04b9a
Tran Khoa <khoatranpc603@gmail.com>
2023-07-08 22:21:04 +0700

    lesson-3

--- lesson-2/controllers/todoList.js
+++ lesson-2/controllers/todoList.js
@@ -1,37 +1,52 @@
-import { todoList } from "../index.js";
+import crypto from 'crypto';
 
 const todoListController = {
-    getAll: (req, res) => {
+    getAll: async (req, res) => {
+        const data = await (await fetch('http://localhost:5001/todoList')).json();
         res.send({
             message: 'Thành công',
-            data: todoList,
+            data: data,
             success: true
         });
     },
-    getOneById: (req, res) => {
+    getOneById: async (req, res) => {
         const { id } = req.params;
-        const crrTodo = todoList.find((item) => {
-            return item.id === id;
-        });
+        const currentTodo = await (await fetch(`http://localhost:5001/todoList/${id}`)).json();
         res.send({
             message: 'Thành công',
-            data: crrTodo,
+            data: currentTodo,
             success: true
         });
     },
-    create: (req, res) => {
-        const { todoName, date } = req.body;
-        const newTodo = {
-            id: crypto.randomUUID(),
-            todoName: todoName,
-            date: date
-        };
-        todoList.push(newTodo);
-        res.send({
-            message: 'Thành công',
-            data: todoList,
-            success: true
-        });
+    create: async (req, res) => {
+        try {
+            const { todoName } = req.body;
+            if (!todoName) throw new Error('Bạn chưa truyền tên công việc (todoName)!');
+            const newTodo = {
+                id: crypto.randomUUID(),
+                todoName: todoName,
+                date: (new Date).getTime()
+            };
+            const insert = await (await fetch('http://localhost:5001/todoList', {
+                method: 'post',
+                body: JSON.stringify(newTodo),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            })).json();
+            if (!insert) throw new Error('Không thành công');
+            res.status(201).send({
+                message: "Thành công!",
+                data: insert,
+                success: false
+            });
+        } catch (error) {
+            res.status(403).send({
+                message: error.message,
+                data: null,
+                success: false
+            });
+        }
     }
 };
 export default todoListController;
--- lesson-2/index.js
+++ lesson-2/index.js
@@ -1,32 +1,8 @@
 import express from 'express';
-import crypto from 'crypto';
 import RootRouterV1 from './routers/v1/index.js';
 
 const app = express();
 app.use(express.json());
-
-export const todoList = [
-    {
-        id: crypto.randomUUID(),
-        todoName: 'Quét nhà',
-        date: new Date()
-    },
-    {
-        id: crypto.randomUUID(),
-        todoName: 'Nấu cơm',
-        date: new Date()
-    },
-    {
-        id: crypto.randomUUID(),
-        todoName: 'Học bài',
-        date: new Date()
-    },
-    {
-        id: crypto.randomUUID(),
-        todoName: 'Đi khám bệnh',
-        date: new Date()
-    },
-]
 
 app.use('/api/v1', RootRouterV1);
 
